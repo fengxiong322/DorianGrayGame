26dfe5e4cb7b95e4be63d5e7c6ea8a85d4d7d44a
Square-1 <41208984+fengxiong322@users.noreply.github.com>
2019-11-07 08:00:06 -1000

    Added basic maze functionality

--- assets/js/maze.js
+++ assets/js/maze.js
@@ -1,6 +1,7 @@
 function maze(){
 
 	/*
+	Position of player is determined from the top left
 	Text input format, 0 indexed. First line contains 2 integers, the players starting position, the next lines contain maze values;
 	*/
 	var map;
@@ -17,11 +18,35 @@ function maze(){
 
 	var gameDiv = document.getElementById('maze');
 	gameDiv.style.display = "block";
-	gameDiv.innerHTML = map;
+	for(int i = 0; i<map.length; i++){
+		gameDiv.innerHTML += map[i] + "\n";
+	}
 	document.addEventListener('keydown', function dodgepress(event){//TODO: add updates to the map, and add vertical control
-	if(event.keyCode == 37) {
-       	posX--;
-    }else if(event.keyCode == 39)
-    	posX++;
-});
+		if(event.keyCode == 37){
+			if(checkMap(map, posX-1, posY))
+       				posX--;
+		}else if(event.keyCode == 39){
+			if(checkMap(map, posX+1, posY))
+    				posX++;
+		}else if(event.keyCode == 38){
+			if(checkMap(map, posX, posY+1))
+				posY++;
+		}else if(event.keyCode == 40){
+			if(checkMap(map, posX-1, posY-1))
+				posY--;
+		}else
+			return;
+		var temp = map;
+		temp[posY]= temp[posY].substr(0, posX) + "@" + temp[posY].substr(posX+1, temp[posY].length());
+		gameDiv.innerHTML = "";
+		for(int i = 0; i<temp.length; i++){
+			gameDiv.innerHTML += temp[i] + "\n";
+		}
+	});
+}
+
+function checkMap(map, posX, posY){
+	if(posX < 0 || posX > map[0].length()-1||posY<0||posY>map.length-1 || map[posY].charAt(posX)=='#')
+		return false;
+	return true;
 }
