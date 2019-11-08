10dca8eddede6d3777a734173173afffaa282a55
Theodore <tpreduta@gmail.com>
2019-11-07 22:10:00 -0500

    fix js formatting part 2

--- assets/js/maze.js
+++ assets/js/maze.js
@@ -1,48 +1,48 @@
 function maze() {
-	/*
-	Position of player is determined from the top left
-	Text input format, 0 indexed. First line contains 2 integers, the players starting position, the next lines contain maze values;
-	*/
-	var input;
-	var client = new XMLHttpRequest();
-	client.open('GET', 'assets/maps/testmap.txt', false);//Deprecated, may need to change
-	client.onreadystatechange = function(){
-		input = client.responseText.split("\n");
-	}
-	client.send();
-	var firstLine = input[0].split(" ");
-	var map = [];
-	for(var i = 1; i < input.length; i++)
-		map.push(input[i]);
-	var posX = parseInt(firstLine[0]);
-	var posY = parseInt(firstLine[1]);
-	document.getElementById('maze').style.display = "block";
-	for(var i = 0; i<map.length; i++){
-		document.getElementById('maze').innerHTML += map[i] + "<br>";
-	}
-	document.addEventListener('keydown', function dodgepress(event){//TODO: add updates to the map, and add vertical control
-		if(event.keyCode == 37){
-			if(checkMap(map, posX-1, posY))
-       				posX--;
-		}else if(event.keyCode == 39){
-			if(checkMap(map, posX+1, posY))
-    				posX++;
-		}else if(event.keyCode == 38){
-			if(checkMap(map, posX, posY-1))
-				posY--;
-		}else if(event.keyCode == 40){
-			if(checkMap(map, posX, posY+1))
-				posY++;
-		}else
-			return;
-		document.getElementById('maze').innerHTML = "";
-		for(var i = 0; i<map.length; i++){
-			if(posY == i)
-				document.getElementById('maze').innerHTML += map[i].substring(0, posX) + "@" + map[i].substring(posX+1, map[i].length)+ "<br>";
-			else
-				document.getElementById('maze').innerHTML += map[i] + "<br>";
-		}
-	});
+  /*
+  Position of player is determined from the top left
+  Text input format, 0 indexed. First line contains 2 integers, the players starting position, the next lines contain maze values;
+  */
+  var input;
+  var client = new XMLHttpRequest();
+  client.open('GET', 'assets/maps/testmap.txt', false);//Deprecated, may need to change
+  client.onreadystatechange = function() {
+    input = client.responseText.split("\n");
+  }
+  client.send();
+  var firstLine = input[0].split(" ");
+  var map = [];
+  for (var i = 1; i < input.length; i++)
+    map.push(input[i]);
+  var posX = parseInt(firstLine[0]);
+  var posY = parseInt(firstLine[1]);
+  document.getElementById('maze').style.display = "block";
+  for (var i = 0; i < map.length; i++) {
+    document.getElementById('maze').innerHTML += map[i] + "<br>";
+  }
+  document.addEventListener('keydown', function dodgepress(event) { //TODO: add updates to the map, and add vertical control
+    if (event.keyCode == 37) {
+      if (checkMap(map, posX-1, posY))
+         posX--;
+    } else if (event.keyCode == 39) {
+      if (checkMap(map, posX+1, posY))
+        posX++;
+    } else if (event.keyCode == 38){
+      if(checkMap(map, posX, posY-1))
+        posY--;
+    } else if (event.keyCode == 40) {
+      if(checkMap(map, posX, posY+1))
+        posY++;
+    } else
+      return;
+    document.getElementById('maze').innerHTML = "";
+    for (var i = 0; i < map.length; i++){
+      if(posY == i)
+        document.getElementById('maze').innerHTML += map[i].substring(0, posX) + "@" + map[i].substring(posX+1, map[i].length)+ "<br>";
+      else
+        document.getElementById('maze').innerHTML += map[i] + "<br>";
+    }
+  });
 }
 
 function checkMap(map, posX, posY) {
